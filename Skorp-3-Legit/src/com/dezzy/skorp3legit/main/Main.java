43a91474201857c9ff2c76844c98d05ecdee384b
Dezz Meister <dezzy3359@gmail.com>
2019-01-13 01:21:48 -0500

    no

--- Skorp-3-Legit/src/com/dezzy/skorp3legit/main/Main.java
+++ Skorp-3-Legit/src/com/dezzy/skorp3legit/main/Main.java
@@ -1,10 +1,12 @@
 package com.dezzy.skorp3legit.main;
 
+import com.dezzy.skorp3legit.test.GLFWTest;
 
 public class Main {
 	
 	public static void main(String[] args) {
-		
+		GLFWTest glfwTest = new GLFWTest();
+		glfwTest.run();
 	}
 	
 }
